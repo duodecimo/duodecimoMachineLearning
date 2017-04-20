fe41d23bc11e6b253d388162659e81bd8773025d
duo <duodecimo@gmail.com>
2017-04-20 01:05:09 -0300

    fixing knn

--- cifar10MachineLearnigCodes/src/ml/KnearestNeighbourgh.java
+++ cifar10MachineLearnigCodes/src/ml/KnearestNeighbourgh.java
@@ -9,7 +9,8 @@ import cifar10.Cifar10Utils;
 import java.io.IOException;
 import org.apache.commons.math3.linear.RealMatrix;
 import static java.lang.Math.abs;
-import static java.lang.Math.abs;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The class KnearestNeighbourgh uses the nearest neighbourgh algorithm to classify
@@ -51,15 +52,15 @@ import static java.lang.Math.abs;
  * @author duo
  */
 public class KnearestNeighbourgh {
-        //  Xtr (of size 50,000 x 32 x 32 x 3) holds all the images in the training set,
-        // and a corresponding 1-dimensional array Ytr (of length 50,000) holds the training labels
-        // (from 0 to 9)
-        // Xtr = TOT_TRAINNINGS x TOT_PIXELS
-        // Xte and Yte do the same for the testing set.
         RealMatrix Xtr, Ytr, Xte, Yte;
         Cifar10Utils cifar10Utils;
 
     public KnearestNeighbourgh(int k) throws IOException {
+        /**
+         * The class cifar10.Cifar10Utils from this package is well documented
+         * and it is strongly recomended the reading of its comments explainning
+         * how it works, what it does and why before going further.
+        */
         cifar10Utils = new Cifar10Utils();
         Xtr = cifar10Utils.getXtr();
         Xte = cifar10Utils.getXte();
@@ -70,31 +71,20 @@ public class KnearestNeighbourgh {
     }
 
     final void kNearestNeighbourEvaluation(int k) throws IOException {
-        // perform k-nearest neighbour
-        // if k=1, it is equivalent to nearest neighbourgh:
-        // The algorithm chooses the label with the least distance between each
-        // test and the train images set.
-        // when k>2, it checks the k least distances between each
-        // test and the train images set, and then chooses the label
-        // with greater number of selections.
-        // When draws happens there must be a tie-braker criteria.
-        // In this case, the natural choice is to pick the minimal
-        // distance between the tie labels. This is why if k = 2, it will
-        // perform the same way as k=1.
         double distance;
         double distanceSwap;
-        double[] minDistance;
-        double[] minTrain;
-        double choosenTrain;
-        double[] minLabel;
-        double choosenLabel;
+        double[] closestDistances;
+        double[] closestTrains;
+        double[] closestLabels;
         double accuracy = 0D;
         double[] sumTr;
         double[] sumTe;
         double[] util;
-        double choosenK;
+        double pickedK;
         int countingK;
         int anteriorCountingK;
+        List<double[]> score;
+        int[] labelAppearances;
         boolean hit = false;
         int maxDisplays = 90;
         // for each image in tests
@@ -102,14 +92,14 @@ public class KnearestNeighbourgh {
         else System.out.println("Running nearestNeighbourgh");
         for(int test = 0; test < Cifar10Utils.TOT_TESTS; test++) {
             sumTe = Xte.getRow(test);
-            minDistance= new double[k]; //Double.MAX_VALUE;
-            minTrain= new double[k]; //Double.MAX_VALUE;
-            minLabel = new double[k];
+            closestDistances= new double[k];
+            closestTrains= new double[k];
+            closestLabels = new double[k];
             // initialization
             for(int i=0; i<k; i++) {
-                minDistance[i] = Double.MAX_VALUE;
-                minTrain[i] = Double.MAX_VALUE;
-                minLabel[i] = Double.MAX_VALUE;
+                closestDistances[i] = Double.MAX_VALUE;
+                closestTrains[i] = Double.MAX_VALUE;
+                closestLabels[i] = Double.MAX_VALUE;
             }
             // calculate distance from each training
             for(int train=0; train < Cifar10Utils.TOT_TRAINNINGS; train++) {
@@ -118,54 +108,66 @@ public class KnearestNeighbourgh {
                 for(int i=0; i<Cifar10Utils.TOT_BYTES; i++) {
                     distance+=((double) abs(sumTe[i]-sumTr[i]));
                 }
+                // select the k closests
                 for (int i = 0; i < k; i++) {
-                    if (distance < minDistance[i]) {
+                    if (distance < closestDistances[i]) {
                         // swap
                         distanceSwap = distance;
-                        distance = minDistance[i];
-                        minDistance[i] = distanceSwap;
+                        distance = closestDistances[i];
+                        closestDistances[i] = distanceSwap;
                         util = Ytr.getRow(train);
-                        minLabel[i] = util[0];
-                        minTrain[i] = train;
+                        closestLabels[i] = util[0];
+                        closestTrains[i] = train;
                     }
                 }
             }
             util = Yte.getRow(test);
             // pick the label that occured most
-            choosenK = 0;
-            countingK = 0;
-            anteriorCountingK = 0;
-            for(int i = 0; i<k-1; i++) {
-                if(minDistance[i+1] == minDistance[i]) {
-                    countingK ++;
-                    if(countingK > anteriorCountingK) {
-                        choosenK = i+1;
-                    }
-                } else {
-                    anteriorCountingK = countingK;
-                    countingK = 1;
+            // lets build a empty array list to hold the score
+            score = new ArrayList<>();
+            // and use ini[] to count the appearances of each score
+            labelAppearances = new int[k];
+            for(int l=0; l<k;l++) labelAppearances[l] = 0;
+            double[] scoreValue = new double[2];
+            for(int i = 0; i<k; i++) {
+                scoreValue[0] = closestLabels[i];
+                scoreValue[1] = closestTrains[i];
+                if(!score.contains(scoreValue)) {
+                    score.add(scoreValue);
+                }
+                labelAppearances[i]++;
+            }
+            // lets check the label with most appearances
+            pickedK = -1;
+            countingK = -1;
+            for(int l = 0; l<scoreValue.length; l++) {
+                if(labelAppearances[l] > countingK) {
+                    pickedK = l;
+                    countingK = labelAppearances[l];
                 }
             }
-            if(minLabel[(int) choosenK] == util[(int) 0]) {
-                accuracy++;
-                if (hit && maxDisplays-->0) {
-                    cifar10Utils.displayImage(Xtr.getRow((int) minTrain[(int) choosenK]), minLabel[(int) choosenK]);
+            // the test was classified with label pickedK!
+            double[]value =  score.get((int) pickedK);
+            if(closestLabels[(int) value[1]] == util[(int) 0]) {
+                if (!hit && maxDisplays-->0) {
+                    cifar10Utils.displayImage(Xtr.getRow((int) closestTrains[(int) pickedK]), closestLabels[(int) pickedK]);
                     cifar10Utils.displayImage(Xte.getRow(test), util[0]);
                     hit = !hit; // toggle
                 }
             } else {
-                if (!hit && maxDisplays-->0) {
-                    cifar10Utils.displayImage(Xtr.getRow((int) minTrain[(int) choosenK]), minLabel[(int) choosenK]);
+                accuracy++;
+                if (hit && maxDisplays-->0) {
+                    cifar10Utils.displayImage(Xtr.getRow((int) closestTrains[(int) pickedK]), closestLabels[(int) pickedK]);
                     cifar10Utils.displayImage(Xte.getRow(test), util[0]);
                     hit = !hit; // toggle
                 }
             }
             // lets display some hits and fails alternating them
-            System.out.println("test: " +test +
-                    " distancia minima: " + minDistance +
-                    " label teste: " + util[0] +
-                    " label minimo: " + minLabel +
-                    "  hittings up to now: " + accuracy);
+            //System.out.println("test: " +test +
+            //        " distancia minima: " + closestDistances +
+            //        " label teste: " + util[0] +
+            //        " label minimo: " + closestLabels +
+            //        "  hittings up to now: " + accuracy);
         }
         System.out.println("Accuracy = " + (accuracy * 100 /(Cifar10Utils.TOT_TESTS)) + "%");
     }
