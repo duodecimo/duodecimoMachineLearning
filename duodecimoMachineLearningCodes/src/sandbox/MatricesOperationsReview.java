74191ea90a3a22a477ad302e7dfd8ead9511f1a9
duo <duodecimo@gmail.com>
2017-04-24 09:51:34 -0300

    extending

--- duodecimoMachineLearningCodes/src/sandbox/MatricesOperationsReview.java
+++ duodecimoMachineLearningCodes/src/sandbox/MatricesOperationsReview.java
@@ -21,24 +21,34 @@ public class MatricesOperationsReview {
         // dimensionality
         System.out.println("Some matrices with several dimensionalities.");
         RealMatrix L1C1 = new Array2DRowRealMatrix(new double[]{1});
-        displayMatrix("lets build a matrix 1 x 1", 
+        MatricesOperationsReview.this.displayRealMatrix("lets build a matrix 1 x 1", 
                 "RealMatrix L1C1 = new Array2DRowRealMatrix(new double[]{1})", "Matrix L1C1:",L1C1);
         RealMatrix L2C1 = new Array2DRowRealMatrix(new double[][]{{1},{2}});
-        displayMatrix("lets build a matrix 2 x 1", 
+        MatricesOperationsReview.this.displayRealMatrix("lets build a matrix 2 x 1", 
                 "RealMatrix L2C1 = new Array2DRowRealMatrix(new double[][]{{1},{2}})" ,"Matrix L2C1:",L2C1);
         RealMatrix L1C2 = new Array2DRowRealMatrix(new double[][]{{1, 2}});
-        displayMatrix("lets build a matrix 1 x 2", 
+        MatricesOperationsReview.this.displayRealMatrix("lets build a matrix 1 x 2", 
                 "RealMatrix L1C2 = new Array2DRowRealMatrix(new double[][]{{1, 2}})", "Matrix L1C2:",L1C2);
         RealMatrix L2C2 = new Array2DRowRealMatrix(new double[][]{{1,2},{3,4}});
-        displayMatrix("lets build a matrix 2 x 2", 
+        MatricesOperationsReview.this.displayRealMatrix("lets build a matrix 2 x 2", 
                 "RealMatrix L2C2 = new Array2DRowRealMatrix(new double[][]{{1,2},{3,4}})", "Matrix L1C1:",L2C2);
+        RealMatrix L2C4 = new Array2DRowRealMatrix(new double[][]{{1,2,3,4},{5,6,7,8}});
+        MatricesOperationsReview.this.displayRealMatrix("lets build a matrix 2 x 4", 
+                "RealMatrix L2C4 = new Array2DRowRealMatrix(new double[][]{{1,2,3,4},{5,6,7,8}})", 
+                "Matrix L2C4:",L2C4);
+        RealMatrix L3C4 = attachOnesRow(L2C4);
+        MatricesOperationsReview.this.displayRealMatrix("lets attach an extra row of ones to it", 
+                "RealMatrix L3C4 = attachOnesRow(L2C4)", 
+                "Matrix L3C4:",L3C4);
+
+/*
         // operations
         System.out.println("Some matrices operations.");
         
         RealMatrix A = new Array2DRowRealMatrix(new double[][]{{1,2,3}, {4,5,6}});
-        displayMatrix("lets build a matrix", "Matrix A:",A);
+        displayRealMatrix("lets build a matrix", "Matrix A:",A);
         try {
-            displayMatrix("let's multiply A to A", "Matrix A * A:", A.multiply(A));
+            displayRealMatrix("let's multiply A to A", "Matrix A * A:", A.multiply(A));
         } catch (DimensionMismatchException dimensionMismatchException) {
             System.out.println("We can't multiply A *A, we get a dimension exception: " + 
                     dimensionMismatchException.getMessage());
@@ -49,47 +59,44 @@ public class MatricesOperationsReview {
                     + "only by a (2, m) matrix!");
             System.out.println("\n");
         }
-        displayMatrix("lets trasnpose it", "Matrix A':",A.transpose());
-        displayMatrix("Now we can multiply both","Matrix A * A':",A.multiply(A.transpose()));
+        displayRealMatrix("lets trasnpose it", "Matrix A':",A.transpose());
+        displayRealMatrix("Now we can multiply both","Matrix A * A':",A.multiply(A.transpose()));
         
-        A.
-
-        /*
         RealMatrix W = new Array2DRowRealMatrix(new double[][]{{0.2, -0.5, 0.1, 2.0},
             {1.5,1.3,2.1,0}, {0,0.25,0.2,-0.3}});
         System.out.println("Display W:");
-        displayMatrix(W);
+        displayRealMatrix(W);
         RealVector x = new ArrayRealVector(new double[]{56, 231, 24, 2});
         System.out.println("Display x:");
-        displayVector(x);
+        displayRealVector(x);
         System.out.println("Display b:");
         RealVector b = new ArrayRealVector(new double[]{1.1, 3.2, -1.2});
-        displayVector(b);
+        displayRealVector(b);
         RealMatrix R = W.multiply(new Array2DRowRealMatrix(x.toArray()));
         System.out.println("Display W * x");
-        displayMatrix(R);
+        displayRealMatrix(R);
         System.out.println("Display W * x + b");
-        displayMatrix(R.add(new Array2DRowRealMatrix(b.toArray())));
+        displayRealMatrix(R.add(new Array2DRowRealMatrix(b.toArray())));
         */
 }
 
-    void displayMatrix(String title, String javaCode, String metadata, RealMatrix M) {
+    void displayRealMatrix(String title, String javaCode, String metadata, RealMatrix M) {
         System.out.println("task: " + title);
         System.out.println("Code: " + javaCode);
-        displayMatrix(metadata, M);
+        MatricesOperationsReview.this.displayRealMatrix(metadata, M);
     }
 
-    void displayMatrix(String title, String metadata, RealMatrix M) {
+    void displayRealMatrix(String title, String metadata, RealMatrix M) {
         System.out.println(title);
-        displayMatrix(metadata, M);
+        MatricesOperationsReview.this.displayRealMatrix(metadata, M);
     }
 
-    void displayMatrix(String metadata, RealMatrix M) {
+    void displayRealMatrix(String metadata, RealMatrix M) {
         System.out.println(metadata + " (" + M.getRowDimension() + " X " + M.getColumnDimension() + ")");
-        displayMatrix(M);
+        displayRealMatrix(M);
     }
 
-    void displayMatrix(RealMatrix M) {
+    void displayRealMatrix(RealMatrix M) {
         for(int l=0; l< M.getRowDimension(); l++) {
             System.out.println("");
             for(int c = 0; c < M.getColumnDimension(); c++) {
@@ -99,12 +106,26 @@ public class MatricesOperationsReview {
         System.out.println("\n");
     }
 
-    void displayVector(RealVector v) {
+    void displayRealVector(RealVector v) {
         for(int l=0; l< v.getDimension(); l++) {
-            System.out.print(String.format(" %8.2f", v.getEntry(l)));
+            System.out.println(String.format(" %8.2f", v.getEntry(l)));
         }
         System.out.println("\n");
     }
+
+    RealMatrix attachOnesRow(RealMatrix M) {
+        int rows = M.getRowDimension();
+        int columns = M.getColumnDimension();
+        double[][] data = M.getData();
+        double[][] newData = new double[rows+1][columns];
+        for(int r=0; r<rows; r++) {
+            System.arraycopy(data[r], 0, newData[r], 0, columns);
+        }
+        for(int c = 0; c<columns; c++) {
+            newData[rows][c] = 1D;
+        }
+        return new Array2DRowRealMatrix(newData);
+    } 
 
     public static void main(String [] args) {
         MatricesOperationsReview matricesOperationsReview = new MatricesOperationsReview();
